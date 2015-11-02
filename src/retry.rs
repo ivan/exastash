bff7262d6c73340a9632eaa44c7e2857c8f90553
Ivan Kozik <ivan@ludios.org>
2015-11-02 14:51:44 +0000

    Make Decayer work

--- src/retry.rs
+++ src/retry.rs
@@ -4,7 +4,7 @@ use num::rational::Ratio;
 
 // We need to be able to multiply a std::time::Duration by a
 // num::rational:Ratio, so we need our own Mul trait.
-trait MyMul<RHS=Self> {
+pub trait MyMul<RHS=Self> {
 	type Output;
 	fn mymul(self, rhs: RHS) -> Self::Output;
 }
@@ -19,7 +19,7 @@ impl MyMul<u32> for Duration {
 impl MyMul<Ratio<u32>> for Duration {
 	type Output = Duration;
 	fn mymul(self, rhs: Ratio<u32>) -> Duration {
-		(self * rhs.numer()) / rhs.denom()
+		(self * *rhs.numer()) / *rhs.denom()
 	}
 }
 
@@ -52,24 +52,22 @@ pub struct Decayer<N: MyMul<M, Output=N> + Ord + Copy, M: Copy> {
 
 impl <N: MyMul<M, Output=N> + Ord + Copy, M: Copy> Decayer<N, M> {
 	pub fn new(initial: N, multiplier: M, max: N) -> Decayer<N, M> {
-		let decayer = Decayer {
+		Decayer {
 			initial: initial,
 			multiplier: multiplier,
 			max: max,
 			first: true,
 			current: initial,
-		};
-		decayer.reset();
-		decayer
+		}
 	}
 
-	pub fn reset(&self) -> N {
+	pub fn reset(&mut self) -> N {
 		self.first = true;
 		self.current = self.initial;
 		self.current
 	}
 
-	pub fn decay(&self) -> N {
+	pub fn decay(&mut self) -> N {
 		if self.first {
 			self.first = false;
 		} else {
@@ -82,10 +80,12 @@ impl <N: MyMul<M, Output=N> + Ord + Copy, M: Copy> Decayer<N, M> {
 #[cfg(test)]
 mod tests {
 	use super::*;
+	use std::time::Duration;
+	use num::rational::Ratio;
 
 	#[test]
-	fn test_decayer() {
-		let decayer: Decayer<u32, u32> = Decayer::new(1, 2, 20);
+	fn test_decayer_u32() {
+		let mut decayer: Decayer<u32, u32> = Decayer::new(1, 2, 20);
 		assert_eq!(decayer.decay(), 1);
 		assert_eq!(decayer.decay(), 2);
 		assert_eq!(decayer.decay(), 4);
@@ -94,5 +94,33 @@ mod tests {
 		assert_eq!(decayer.decay(), 20);
 		assert_eq!(decayer.decay(), 20);
 		assert_eq!(decayer.reset(), 1);
+	}
+
+	#[test]
+	fn test_decayer_u64() {
+		let mut decayer: Decayer<u64, u64> = Decayer::new(1u64, 2u64, 20u64);
+		assert_eq!(decayer.decay(), 1);
+		assert_eq!(decayer.decay(), 2);
+		assert_eq!(decayer.decay(), 4);
+		assert_eq!(decayer.decay(), 8);
+		assert_eq!(decayer.decay(), 16);
+		assert_eq!(decayer.decay(), 20);
+		assert_eq!(decayer.decay(), 20);
+		assert_eq!(decayer.reset(), 1);
+	}
+
+	#[test]
+	fn test_decayer_duration() {
+		let mut decayer: Decayer<Duration, Ratio<u32>> = Decayer::new(
+			Duration::new(1, 0),
+			Ratio::new(3, 2),
+			Duration::new(4, 0)
+		);
+		assert_eq!(decayer.decay(), Duration::new(1, 0));
+		assert_eq!(decayer.decay(), Duration::new(1, 500000000));
+		assert_eq!(decayer.decay(), Duration::new(2, 250000000));
+		assert_eq!(decayer.decay(), Duration::new(3, 375000000));
+		assert_eq!(decayer.decay(), Duration::new(4, 0));
+		assert_eq!(decayer.reset(), Duration::new(1, 0));
 	}
 }
