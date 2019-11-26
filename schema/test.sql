595607e70689721eadfcc67fac87b4a2aad7b5ba
Ivan Kozik <ivan@ludios.org>
2019-11-26 15:24:49 +0000

    Add test

--- schema/test.sql
+++ schema/test.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-SELECT plan(15);
+SELECT plan(16);
 SELECT has_table('inodes');
 
 PREPARE cannot_insert_with_negative_ino AS INSERT INTO inodes (
@@ -57,6 +57,11 @@ PREPARE cannot_insert_dir_with_symlink_target AS INSERT INTO inodes (
   type, size, mtime, executable, inline_content, symlink_target
 ) VALUES ('DIR', NULL, (0, 0), NULL, NULL, '../some/target');
 SELECT throws_ilike('cannot_insert_dir_with_symlink_target', '%violates check constraint%');
+
+PREPARE cannot_insert_reg_with_wrong_size AS INSERT INTO inodes (
+  type, size, mtime, executable, inline_content, symlink_target
+) VALUES ('REG', 6, (0, 0), true, 'hello', NULL);
+SELECT throws_ilike('cannot_insert_reg_with_wrong_size', '%violates check constraint%');
 
 PREPARE insert_reg_with_inline_content AS INSERT INTO inodes (
   type, size, mtime, executable, inline_content, symlink_target
