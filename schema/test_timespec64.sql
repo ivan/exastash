3bdf381777fdbb69ddcde1cdcd5507c9a944dac0
Ivan Kozik <ivan@ludios.org>
2019-11-30 17:22:57 +0000

    Add more timespec64 tests

--- schema/test_timespec64.sql
+++ schema/test_timespec64.sql
@@ -1,8 +1,12 @@
 BEGIN;
 
-SELECT plan(1);
+SELECT plan(3);
 
 SELECT ok('epoch'::timestamptz::timespec64 = (0, 0)::timespec64);
+
+SELECT ok(('epoch'::timestamptz + '1 hour'::interval)::timespec64 = (3600, 0)::timespec64);
+
+SELECT ok(('epoch'::timestamptz + '1 hour 1 microsecond'::interval)::timespec64 = (3600, 1000)::timespec64);
 
 SELECT * FROM finish();
 
