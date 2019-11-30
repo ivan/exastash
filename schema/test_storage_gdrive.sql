e82f8417f3f254cfaac7cb76e84468a62dbd2ed3
Ivan Kozik <ivan@ludios.org>
2019-11-30 21:46:20 +0000

    schema: rework some of storage_gdrive; add cipher/cipher_key

--- schema/test_storage_gdrive.sql
+++ schema/test_storage_gdrive.sql
@@ -2,46 +2,46 @@ BEGIN;
 
 SELECT plan(14);
 
-INSERT INTO gdrive_domains (gdrive_domain) VALUES ('example.org');
+INSERT INTO gsuite_domains (gsuite_domain) VALUES ('example.org');
 INSERT INTO gdrive_files (file_id, file_owner, md5, crc32c, size)
     VALUES ('AAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 'some@user', '\xd41d8cd98f00b204e9800998ecf8427e', '\x00000000', 1);
 INSERT INTO gdrive_files (file_id, file_owner, md5, crc32c, size)
     VALUES ('AAAAAAAAAAAAAAAAAAAAAAAAAAAA2', 'some@user', '\xd41d8cd98f00b204e9800998ecf8427e', '\x00000000', 1);
 
-PREPARE cannot_create_empty_chunk_sequence AS INSERT INTO gdrive_chunk_sequences (files, aes_key)
-    VALUES (ARRAY[]::file_id[], '\x00000000000000000000000000000000');
+PREPARE cannot_create_empty_chunk_sequence AS INSERT INTO gdrive_chunk_sequences (files, cipher, cipher_key)
+    VALUES (ARRAY[]::file_id[], 'AES_128_GCM', '\x00000000000000000000000000000000');
 SELECT throws_ilike('cannot_create_empty_chunk_sequence', '%violates check constraint%');
 
-PREPARE cannot_reference_nonexistent_files AS INSERT INTO gdrive_chunk_sequences (files, aes_key)
-    VALUES (ARRAY['BAAAAAAAAAAAAAAAAAAAAAAAAAAAA']::file_id[], '\x00000000000000000000000000000000');
+PREPARE cannot_reference_nonexistent_files AS INSERT INTO gdrive_chunk_sequences (files, cipher, cipher_key)
+    VALUES (ARRAY['BAAAAAAAAAAAAAAAAAAAAAAAAAAAA']::file_id[], 'AES_128_GCM', '\x00000000000000000000000000000000');
 SELECT throws_ilike('cannot_reference_nonexistent_files', '%only 0 of these are in gdrive_files%');
 
-PREPARE cannot_reference_nonexistent_files2 AS INSERT INTO gdrive_chunk_sequences (files, aes_key)
-    VALUES (ARRAY['AAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 'BAAAAAAAAAAAAAAAAAAAAAAAAAAAA']::file_id[], '\x00000000000000000000000000000000');
+PREPARE cannot_reference_nonexistent_files2 AS INSERT INTO gdrive_chunk_sequences (files, cipher, cipher_key)
+    VALUES (ARRAY['AAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 'BAAAAAAAAAAAAAAAAAAAAAAAAAAAA']::file_id[], 'AES_128_GCM', '\x00000000000000000000000000000000');
 SELECT throws_ilike('cannot_reference_nonexistent_files2', '%only 1 of these are in gdrive_files%');
 
-PREPARE cannot_reference_same_file_more_than_once AS INSERT INTO gdrive_chunk_sequences (files, aes_key)
-    VALUES (ARRAY['AAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAA']::file_id[], '\x00000000000000000000000000000000');
+PREPARE cannot_reference_same_file_more_than_once AS INSERT INTO gdrive_chunk_sequences (files, cipher, cipher_key)
+    VALUES (ARRAY['AAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 'AAAAAAAAAAAAAAAAAAAAAAAAAAAAA']::file_id[], 'AES_128_GCM', '\x00000000000000000000000000000000');
 SELECT throws_ilike('cannot_reference_same_file_more_than_once', '%only 1 of these are in gdrive_files%');
 
-PREPARE cannot_insert_without_aes_key AS INSERT INTO gdrive_chunk_sequences (files)
+PREPARE cannot_insert_without_cipher_key AS INSERT INTO gdrive_chunk_sequences (files)
     VALUES (ARRAY['AAAAAAAAAAAAAAAAAAAAAAAAAAAAA']::file_id[]);
-SELECT throws_ilike('cannot_insert_without_aes_key', '%violates not-null constraint%');
+SELECT throws_ilike('cannot_insert_without_cipher_key', '%violates not-null constraint%');
 
-PREPARE cannot_insert_wrong_aes_key_size_15 AS INSERT INTO gdrive_chunk_sequences (files, aes_key)
-    VALUES (ARRAY['AAAAAAAAAAAAAAAAAAAAAAAAAAAAA']::file_id[], '\x000000000000000000000000000000');
-SELECT throws_ilike('cannot_insert_wrong_aes_key_size_15', '%violates check constraint%');
+PREPARE cannot_insert_wrong_cipher_key_size_15 AS INSERT INTO gdrive_chunk_sequences (files, cipher, cipher_key)
+    VALUES (ARRAY['AAAAAAAAAAAAAAAAAAAAAAAAAAAAA']::file_id[], 'AES_128_GCM', '\x000000000000000000000000000000');
+SELECT throws_ilike('cannot_insert_wrong_cipher_key_size_15', '%violates check constraint%');
 
-PREPARE cannot_insert_wrong_aes_key_size_17 AS INSERT INTO gdrive_chunk_sequences (files, aes_key)
-    VALUES (ARRAY['AAAAAAAAAAAAAAAAAAAAAAAAAAAAA']::file_id[], '\x0000000000000000000000000000000000');
-SELECT throws_ilike('cannot_insert_wrong_aes_key_size_17', '%violates check constraint%');
+PREPARE cannot_insert_wrong_cipher_key_size_17 AS INSERT INTO gdrive_chunk_sequences (files, cipher, cipher_key)
+    VALUES (ARRAY['AAAAAAAAAAAAAAAAAAAAAAAAAAAAA']::file_id[], 'AES_128_GCM', '\x0000000000000000000000000000000000');
+SELECT throws_ilike('cannot_insert_wrong_cipher_key_size_17', '%violates check constraint%');
 
-PREPARE one_file_sequence AS INSERT INTO gdrive_chunk_sequences (files, aes_key)
-    VALUES (ARRAY['AAAAAAAAAAAAAAAAAAAAAAAAAAAAA']::file_id[], '\x00000000000000000000000000000000');
+PREPARE one_file_sequence AS INSERT INTO gdrive_chunk_sequences (files, cipher, cipher_key)
+    VALUES (ARRAY['AAAAAAAAAAAAAAAAAAAAAAAAAAAAA']::file_id[], 'AES_128_GCM', '\x00000000000000000000000000000000');
 SELECT lives_ok('one_file_sequence');
 
-PREPARE two_file_sequence AS INSERT INTO gdrive_chunk_sequences (files, aes_key)
-    VALUES (ARRAY['AAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 'AAAAAAAAAAAAAAAAAAAAAAAAAAAA2']::file_id[], '\x00000000000000000000000000000001');
+PREPARE two_file_sequence AS INSERT INTO gdrive_chunk_sequences (files, cipher, cipher_key)
+    VALUES (ARRAY['AAAAAAAAAAAAAAAAAAAAAAAAAAAAA', 'AAAAAAAAAAAAAAAAAAAAAAAAAAAA2']::file_id[], 'AES_128_CTR', '\x00000000000000000000000000000001');
 SELECT lives_ok('two_file_sequence');
 
 -- Files can't be deleted if they're still referenced by a sequence
