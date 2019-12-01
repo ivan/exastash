f4a6bca1f6063108deb2bd5b0d0b6133b57bc9cd
Ivan Kozik <ivan@ludios.org>
2019-12-01 22:21:26 +0000

    schema: remove unnecessary ::timespec64

--- schema/test_storage_inline.sql
+++ schema/test_storage_inline.sql
@@ -13,7 +13,7 @@ SELECT throws_ilike('cannot_add_content_for_dir', '%inode 2 is DIR, not a regula
 -- Successes
 
 INSERT INTO inodes (ino, type, size, mtime, executable, birth_time, birth_hostname, birth_exastash_version)
-    VALUES (100, 'REG', 11, now()::timespec64, false, now()::timespec64, 'fake', 41);
+    VALUES (100, 'REG', 11, now(), false, now(), 'fake', 41);
 
 PREPARE insert_content AS INSERT INTO storage_inline (ino, content) VALUES (100, E'hello\nworld');
 SELECT lives_ok('insert_content');
