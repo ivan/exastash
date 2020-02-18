76f921462228c56cda0059287e4726fc3d01a058
Ivan Kozik <ivan@ludios.org>
2020-02-18 18:22:17 +0000

    Refactor a bit

--- src/db.rs
+++ src/db.rs
@@ -7,7 +7,7 @@ fn env_var(var: &str) -> Result<String> {
 }
 
 fn postgres_client_production() -> Result<Client> {
-    let database_url = env_var("DATABASE_URL")?;
+    let database_url = env_var("EXASTASH_POSTGRESQL_URL")?;
     Ok(Client::connect(&database_url, NoTls)?)
 }
 
@@ -17,7 +17,7 @@ mod tests {
     use once_cell::sync::Lazy;
     use std::process::Command;
 
-    static PG_TEMP_URL: Lazy<String> = Lazy::new(|| {
+    static DATABASE_URL: Lazy<String> = Lazy::new(|| {
         let mut pg_tmp = Command::new("pg_tmp");
         let stdout = pg_tmp.output().expect("failed to execute pg_tmp").stdout;
         let database_url = String::from_utf8(stdout).expect("could not parse pg_tmp output as UTF-8");
@@ -28,9 +28,13 @@ mod tests {
         database_url
     });
 
+    fn get_client() -> Client {
+        Client::connect(&*DATABASE_URL, NoTls).unwrap()
+    }
+
     #[test]
     fn test_cannot_change_dir_immutables() -> Result<()> {
-        let mut client = Client::connect(&*PG_TEMP_URL, NoTls).unwrap();
+        let mut client = get_client();
         let mut transaction = client.transaction()?;
         transaction.execute("SET search_path TO stash", &[])?;
         transaction.commit()?;
