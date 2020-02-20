a864d93227e930fbfbf6e0b8750f45460eaf8949
Ivan Kozik <ivan@ludios.org>
2020-02-20 20:31:20 +0000

    db::dirents: factor out an InodeTuple

--- src/db/dirent.rs
+++ src/db/dirent.rs
@@ -2,31 +2,30 @@ use crate::db::inode::Inode;
 use anyhow::{bail, Result};
 use postgres::Transaction;
 
-trait InodeTuple {
-    fn from_tuple(child_dir: Option<i64>, child_file: Option<i64>, child_symlink: Option<i64>) -> Inode;
-    fn to_tuple(self) -> (Option<i64>, Option<i64>, Option<i64>);
-}
+/// A (dir, file, symlink) tuple that is useful when interacting with
+/// the dirents table.
+#[derive(Debug)]
+struct InodeTuple(Option<i64>, Option<i64>, Option<i64>);
 
-impl InodeTuple for Inode {
-    /// Converts a (dir, file, symlink) tuple from the database.
-    /// Exactly one must be Some, else this panics.
-    fn from_tuple(child_dir: Option<i64>, child_file: Option<i64>, child_symlink: Option<i64>) -> Inode {
-        let tuple = (child_dir, child_file, child_symlink);
-        match tuple {
-            (Some(id), None, None) => Inode::Dir(id),
-            (None, Some(id), None) => Inode::File(id),
-            (None, None, Some(id)) => Inode::Symlink(id),
-            _                      => panic!("unexpected tuple {:?}", tuple),
+impl InodeTuple {
+    /// Converts an InodeTuple to an Inode.
+    /// Exactly one value must be Some, else this panics.
+    fn to_inode(self) -> Inode {
+        match self {
+            InodeTuple(Some(id), None, None) => Inode::Dir(id),
+            InodeTuple(None, Some(id), None) => Inode::File(id),
+            InodeTuple(None, None, Some(id)) => Inode::Symlink(id),
+            _                                => panic!("tuple {:?} does not have exactly 1 Some", self),
         }
     }
 
-    /// Returns a (dir, file, symlink) tuple for use with the database.
-    /// One will be Some, the rest will be None.
-    fn to_tuple(self) -> (Option<i64>, Option<i64>, Option<i64>) {
-        match self {
-            Inode::Dir(id)     => (Some(id), None, None),
-            Inode::File(id)    => (None, Some(id), None),
-            Inode::Symlink(id) => (None, None, Some(id)),
+    /// Converts an Inode to an InodeTuple.
+    /// One value will be Some, the rest will be None.
+    fn from_inode(inode: Inode) -> InodeTuple {
+        match inode {
+            Inode::Dir(id)     => InodeTuple(Some(id), None, None),
+            Inode::File(id)    => InodeTuple(None, Some(id), None),
+            Inode::Symlink(id) => InodeTuple(None, None, Some(id)),
         }
     }
 }
@@ -50,7 +49,7 @@ pub(crate) fn create_dirent(transaction: &mut Transaction, parent: Inode, dirent
         Inode::Dir(id) => id,
         _ => bail!("parent must be a directory"),
     };
-    let (child_dir, child_file, child_symlink) = dirent.child.to_tuple();
+    let InodeTuple(child_dir, child_file, child_symlink) = InodeTuple::from_inode(dirent.child);
     transaction.execute(
         "INSERT INTO dirents (parent, basename, child_dir, child_file, child_symlink)
          VALUES ($1::bigint, $2::text, $3::bigint, $4::bigint, $5::bigint)",
@@ -71,10 +70,8 @@ pub(crate) fn list_dir(transaction: &mut Transaction, parent: Inode) -> Result<V
     let mut out = vec![];
     for row in rows {
         let basename: String = row.get(0);
-        let child_dir: Option<i64> = row.get(1);
-        let child_file: Option<i64> = row.get(2);
-        let child_symlink: Option<i64> = row.get(3);
-        let inode = Inode::from_tuple(child_dir, child_file, child_symlink);
+        let tuple = InodeTuple(row.get(1), row.get(2), row.get(3));
+        let inode = tuple.to_inode();
         let dirent = Dirent::new(basename, inode);
         out.push(dirent);
     }
