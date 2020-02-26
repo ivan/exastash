08dd2c0a5b536cbe095c9f8974fff3a2b5c506f8
Ivan Kozik <ivan@ludios.org>
2020-02-26 06:57:16 +0000

    Enable more warnings
    Cargo-culted from https://github.com/astral-engine/astral/blob/036023880fe1ad1559067ba081340016d1cd9836/src/math/src/lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,21 @@
 //! exastash, a filesystem for archiving petabytes
 
-#![warn(missing_docs, rust_2018_idioms, clippy::all)]
+#![warn(
+	nonstandard_style,
+	rust_2018_compatibility,
+	rust_2018_idioms,
+	unused,
+	macro_use_extern_crate,
+	missing_copy_implementations,
+	missing_debug_implementations,
+	missing_docs,
+	trivial_casts,
+	trivial_numeric_casts,
+	unused_import_braces,
+	unused_lifetimes,
+	variant_size_differences,
+	clippy::pedantic
+)]
 // TODO: remove after initial development
 #![allow(dead_code)]
 #![allow(unused_variables)]
