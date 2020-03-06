7d1bb15d49fc998447b1ec4ba00ecdaed9156643
Ivan Kozik <ivan@ludios.org>
2020-03-06 05:07:38 +0000

    Implement db::storage::get_storage

--- src/db/storage.rs
+++ src/db/storage.rs
@@ -6,6 +6,7 @@ use anyhow::Result;
 use postgres::Transaction;
 use crate::db::inode::Inode;
 
+#[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Storage {
     Inline(inline::Storage),
     Gdrive(gdrive::Storage),
@@ -16,9 +17,17 @@ pub enum Storage {
 pub fn get_storage(transaction: &mut Transaction<'_>, inode: Inode) -> Result<Vec<Storage>> {
     let file_id = inode.file_id();
 
-    let inline = inline::get_storage(transaction, inode)?;
-    let gdrive = gdrive::get_storage(transaction, inode)?;
-    let internetarchive = internetarchive::get_storage(transaction, inode)?;
+    transaction.execute("SET TRANSACTION ISOLATION LEVEL REPEATABLE READ", &[])?;
+    let inline = inline::get_storage(transaction, inode)?
+        .into_iter().map(Storage::Inline).collect::<Vec<_>>();
+    let gdrive = gdrive::get_storage(transaction, inode)?
+        .into_iter().map(Storage::Gdrive).collect::<Vec<_>>();
+    let internetarchive = internetarchive::get_storage(transaction, inode)?
+        .into_iter().map(Storage::InternetArchive).collect::<Vec<_>>();
 
-    Ok(vec![])
+    Ok([
+        &inline[..],
+        &gdrive[..],
+        &internetarchive[..],
+    ].concat())
 }
