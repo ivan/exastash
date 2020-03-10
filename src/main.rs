61934ebdf3eab52e56ae033f8643bc5f3d45f14b
Ivan Kozik <ivan@ludios.org>
2020-03-10 05:52:05 +0000

    WIP ls command

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,24 @@ use structopt::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "es")]
 /// exastash
-enum Opt {
+enum Command {
     #[structopt(name = "ls")]
     /// List files in a stash directory
     Ls {
+        /// Path
+        #[structopt(name = "PATH")]
+        path: String,
+
         #[structopt(short = "j")]
-        /// Print just the filenames without any decoration
+        /// Print just the filenames
         just_names: bool,
     },
 }
 
 fn main() {
-    let matches = Opt::from_args();
-
-    println!("{:?}", matches);
+    match Command::from_args() {
+        Command::Ls { path, just_names } => {
+            dbg!(path, just_names);
+        }
+    }
 }
