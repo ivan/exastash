a2d3882fd0d3866e79c3a5d9b308a8b408ff08af
Ivan Kozik <ivan@ludios.org>
2020-04-18 10:27:31 +0000

    Finish stream_gdrive_file

--- src/gdrive.rs
+++ src/gdrive.rs
@@ -18,7 +18,7 @@ async fn get_token_for_service_account<P: AsRef<Path>>(json_path: P) -> Result<A
 
 // Take AsRef<str> instead of AccessToken because AccessToken has private fields
 // and we can't construct a fake one in tests
-async fn stream_gdrive_file<T: AsRef<str> + Display>(access_token: T, file_id: &str) -> Result<()> {
+async fn stream_gdrive_file<T: AsRef<str> + Display>(access_token: T, file_id: &str) -> Result<impl tokio::io::AsyncRead> {
     static FILE_ID_RE: &Lazy<Regex> = lazy_regex!(r#"\A[-_0-9A-Za-z]{28,160}\z"#);
     if let None = FILE_ID_RE.captures(file_id) {
         bail!("Invalid gdrive file_id: {:?}", file_id);
@@ -30,30 +30,18 @@ async fn stream_gdrive_file<T: AsRef<str> + Display>(access_token: T, file_id: &
         .header("Authorization", format!("Bearer {}", access_token))
         .send()
         .await?;
-    match response.status() {
+    Ok(match response.status() {
         StatusCode::OK => {
-            // Convert the body of the response into a futures::io::Stream.
-            let body = response.bytes_stream();
-
-            // Convert the stream into an futures::io::AsyncRead.
-            // We must first convert the reqwest::Error into an futures::io::Error.
-            let body = body
+            response
+                .bytes_stream()
                 .map_err(|e| futures::io::Error::new(futures::io::ErrorKind::Other, e))
-                .into_async_read();
-
-            // Convert the futures::io::AsyncRead into a tokio::io::AsyncRead.
-            let mut body = body.compat();
-
-            tokio::io::copy(&mut body, &mut tokio::io::stdout()).await?;
-        },
-        StatusCode::NOT_FOUND => {
-            bail!("{} responded with HTTP status code {}", url, response.status());
+                .into_async_read()
+                .compat()
         },
         _ => {
             bail!("{} responded with HTTP status code {}", url, response.status());
         }
-    }
-    Ok(())
+    })
 }
 
 #[cfg(test)]
