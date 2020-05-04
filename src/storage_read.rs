db588aeae03b987d80552b15d8bc4d4883cf6354
Ivan Kozik <ivan@ludios.org>
2020-05-04 10:38:04 +0000

    WIP storage_read

--- src/storage_read.rs
+++ src/storage_read.rs
@@ -1,40 +1,14 @@
-use anyhow::Result;
-use bytes::Bytes;
+use anyhow::{Result, Error};
 use tokio_util::compat::FuturesAsyncReadCompatExt;
-use futures::stream::{Stream, TryStreamExt};
-use std::pin::Pin;
-use std::task::{Context, Poll};
+use futures::stream::{self,  TryStreamExt};
 use crate::db::storage::{Storage, inline, gdrive, internetarchive};
 
-struct SingleItemStream {
-    content: Vec<u8>,
-    done: bool,
-}
-
-impl SingleItemStream {
-    fn new(content: Vec<u8>) -> Self {
-        SingleItemStream { content, done: false }
-    }
-}
-
-impl Stream for SingleItemStream {
-    type Item = Result<Bytes>;
-
-    fn poll_next(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
-        if self.done {
-            return Poll::Ready(None);
-        }
-        self.done = true;
-        // TODO: avoid a clone here?
-        Poll::Ready(Some(Ok(Bytes::from(self.content.clone()))))
-    }
-}
-
-pub(crate) fn read(storage: &Storage) -> Result<impl tokio::io::AsyncRead> {
-    Ok(match storage {
+pub(crate) fn read(storage: &Storage) -> Result<Box<dyn tokio::io::AsyncRead>> {
+    Ok(Box::new(match storage {
         Storage::Inline(inline::Storage { content, .. }) => {
-            SingleItemStream::new(content.clone())
-                .map_err(|e| futures::io::Error::new(futures::io::ErrorKind::Other, e))
+            let stream = stream::iter::<_>(vec![Ok(content.clone())]);
+            stream
+                .map_err(|e: Error| futures::io::Error::new(futures::io::ErrorKind::Other, e))
                 .into_async_read()
                 .compat()
         }
@@ -44,5 +18,5 @@ pub(crate) fn read(storage: &Storage) -> Result<impl tokio::io::AsyncRead> {
         Storage::InternetArchive(internetarchive::Storage { .. }) => {
             unimplemented!()
         }
-    })
+    }))
 }
