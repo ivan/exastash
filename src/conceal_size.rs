f79e813505e0f88c91aea726bf5753683d97951f
Ivan Kozik <ivan@ludios.org>
2020-06-28 22:43:23 +0000

    conceal_size: halve the maximum and average wasteage

--- src/conceal_size.rs
+++ src/conceal_size.rs
@@ -24,15 +24,15 @@ fn round_up_to_nearest(n: u64, nearest: u64) -> u64 {
 }
 
 /// For tiny files (< 2KB), return 16
-/// For non-tiny files, return (2^floor(log2(n)))/64
+/// For non-tiny files, return (2^floor(log2(n)))/128
 pub(crate) fn get_concealment_size(n: u64) -> u64 {
-    // Use an average wasteage of 1/128 (~.78%) and max wasteage of 1/64
-    let ret = 2_u64.pow(floor_log2(n)) / 64;
+    // Use an average wasteage of 1/256 (~.39%) and max wasteage of 1/128
+    let ret = 2_u64.pow(floor_log2(n)) / 128;
     max(16, ret)
 }
 
 /// Conceal a file size by rounding the size up log2-proportionally,
-/// to a size 0% to 1.5625% of the original size.
+/// to a size 0% to 0.78125% of the original size.
 pub(crate) fn conceal_size(n: u64) -> u64 {
     let ret = round_up_to_nearest(max(1, n), get_concealment_size(n));
     assert!(ret >= n);
@@ -51,17 +51,15 @@ mod tests {
         assert_eq!(get_concealment_size(256), 16);
         assert_eq!(get_concealment_size(1024), 16);
         assert_eq!(get_concealment_size(1536), 16);
-        assert_eq!(get_concealment_size(2*1024), 32);
-        assert_eq!(get_concealment_size(128*1024), 2048);
+        assert_eq!(get_concealment_size(2*1024), 16);
+        assert_eq!(get_concealment_size(128*1024), 1024);
 
-        assert_eq!(get_concealment_size(1024), 1024/64);
+        assert_eq!(get_concealment_size(1024*1024), 1024*1024/128);
 
-        assert_eq!(get_concealment_size(1024*1024), 1024*1024/64);
-
-        assert_eq!(get_concealment_size(1024*1024*1024 - 1), 1024*1024*1024/128);
-        assert_eq!(get_concealment_size(1024*1024*1024), 1024*1024*1024/64);
-        assert_eq!(get_concealment_size(1024*1024*1024 + 1), 1024*1024*1024/64);
-        assert_eq!(get_concealment_size(1024*1024*1024 + 1024*1024), 1024*1024*1024/64);
+        assert_eq!(get_concealment_size(1024*1024*1024 - 1), 1024*1024*1024/256);
+        assert_eq!(get_concealment_size(1024*1024*1024), 1024*1024*1024/128);
+        assert_eq!(get_concealment_size(1024*1024*1024 + 1), 1024*1024*1024/128);
+        assert_eq!(get_concealment_size(1024*1024*1024 + 1024*1024), 1024*1024*1024/128);
     }
 
     #[test]
@@ -74,11 +72,11 @@ mod tests {
         assert_eq!(conceal_size(1025), 1024 + 16);
         assert_eq!(conceal_size(1536), 1536);
         assert_eq!(conceal_size(2*1024), 2*1024);
-        assert_eq!(conceal_size(2*1024+1), 2*1024 + 32);
+        assert_eq!(conceal_size(2*1024+1), 2*1024 + 16);
 
         assert_eq!(conceal_size(1024*1024*1024 - 1), 1024*1024*1024);
         assert_eq!(conceal_size(1024*1024*1024), 1024*1024*1024);
-        assert_eq!(conceal_size(1024*1024*1024 + 1), 1024*1024*1024 + 1024*1024*1024/64);
-        assert_eq!(conceal_size(1024*1024*1024 + 1024*1024), 1024*1024*1024 + 1024*1024*1024/64);
+        assert_eq!(conceal_size(1024*1024*1024 + 1), 1024*1024*1024 + 1024*1024*1024/128);
+        assert_eq!(conceal_size(1024*1024*1024 + 1024*1024), 1024*1024*1024 + 1024*1024*1024/128);
     }
 }
