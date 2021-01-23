3dc1edaca75dc6c9d2cde15863da1b6645b88cc5
Ivan Kozik <ivan@ludios.org>
2021-01-23 08:54:43 +0000

    new_pgpool: add argument for specifying search_path

--- src/db.rs
+++ src/db.rs
@@ -9,6 +9,7 @@ pub mod google_auth;
 use anyhow::Result;
 use sqlx::Executor;
 use log::LevelFilter;
+use std::sync::Arc;
 use sqlx::postgres::{PgPool, PgPoolOptions, PgConnectOptions};
 use sqlx::{ConnectOptions, Postgres, Transaction, Row};
 use futures::future::{FutureExt, Shared};
@@ -21,21 +22,27 @@ use std::env;
 use crate::util::env_var;
 
 /// Return a `PgPool` that connects to the given `postgres://` URI,
-/// and starts all transactions in `search_path` = `stash` and with
+/// and starts all transactions in `search_path` = search_path and with
 /// isolation level `REPEATABLE READ`.
-pub async fn new_pgpool(uri: &str, max_connections: u32) -> Result<PgPool> {
+pub async fn new_pgpool(uri: &str, max_connections: u32, search_path: &str) -> Result<PgPool> {
+    let search_path = Arc::new(String::from(search_path));
     let mut options: PgConnectOptions = uri.parse()?;
     // By default, sqlx logs statements that take > 1 sec as a warning
     options.log_slow_statements(LevelFilter::Info, Duration::from_secs(5));
     let pool = PgPoolOptions::new()
-        .after_connect(|conn| Box::pin(async move {
-            conn.execute("SET search_path TO stash").await?;
-            // We generally want point-in-time consistency, e.g. when we do separate
-            // reads on files and a storage table
-            conn.execute("SET default_transaction_isolation TO 'repeatable read'").await?;
+        .after_connect(move |conn| {
+            let search_path = search_path.clone();
+            Box::pin(async move {
+                let stmt = format!("SET search_path TO {search_path}");
+                conn.execute(stmt.as_str()).await?;
 
-            Ok(())
-        }))
+                // We generally want point-in-time consistency, e.g. when we do separate
+                // reads on files and a storage table
+                conn.execute("SET default_transaction_isolation TO 'repeatable read'").await?;
+
+                Ok(())
+            })
+        })
         .connect_timeout(Duration::from_secs(30))
         .max_connections(max_connections)
         .connect_with(options).await?;
@@ -54,7 +61,7 @@ static PGPOOL: Lazy<Shared<Pin<Box<dyn Future<Output=PgPool> + Send>>>> = Lazy::
         panic!("Refusing to create pgpool to EXASTASH_POSTGRES_URI={} in tests", database_uri);
     }
 
-    new_pgpool(&database_uri, max_connections).await.unwrap()
+    new_pgpool(&database_uri, max_connections, "stash").await.unwrap()
 }.boxed().shared());
 
 /// Return the global `PgPool`.  It must not be used in more than one tokio runtime.
@@ -131,7 +138,7 @@ pub mod tests {
     /// Return a new `PgPool` connected to the `pg_tmp` for most tests.
     /// We do not return a shared `PgPool` because each `#[tokio::test]` has its own tokio runtime.
     pub(crate) async fn new_primary_pool() -> PgPool {
-        new_pgpool(&*PRIMARY_POOL_URI, 4).await.unwrap()
+        new_pgpool(&*PRIMARY_POOL_URI, 4, "stash").await.unwrap()
     }
 
     /// PgPool Future initialized once by the first caller
@@ -144,6 +151,6 @@ pub mod tests {
     /// Return a new `PgPool` connected to the pg_tmp for `TRUNCATE` tests.
     /// We do not return a shared `PgPool` because each `#[tokio::test]` has its own tokio runtime.
     pub(crate) async fn new_secondary_pool() -> PgPool {
-        new_pgpool(&*SECONDARY_POOL_URI, 4).await.unwrap()
+        new_pgpool(&*SECONDARY_POOL_URI, 4, "stash").await.unwrap()
     }
 }
