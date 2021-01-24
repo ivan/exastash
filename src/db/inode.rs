ivan/exastash

commit 9cfd9051623efd86cfcaac7c649c69bb39e5b1f0

Use explicit schema in queries so that applications can use exastash while having their own arbitrary search_path

src/db/inode.rs
@@ -102,7 +102,7 @@ impl Dir {
         if ids.is_empty() {
             return Ok(vec![])
         }
-        let query = "SELECT id, mtime, birth_time, birth_version, birth_hostname FROM dirs WHERE id = ANY($1::bigint[])";
+        let query = "SELECT id, mtime, birth_time, birth_version, birth_hostname FROM stash.dirs WHERE id = ANY($1::bigint[])";
         Ok(sqlx::query_as::<_, Dir>(query).bind(ids).fetch_all(transaction).await?)
     }
 
@@ -113,7 +113,7 @@ impl Dir {
     ///
     /// Does not commit the transaction, you must do so yourself.
     pub async fn remove(transaction: &mut Transaction<'_, Postgres>, ids: &[i64]) -> Result<()> {
-        let stmt = "DELETE FROM dirs WHERE id = ANY($1::bigint[])";
+        let stmt = "DELETE FROM stash.dirs WHERE id = ANY($1::bigint[])";
         sqlx::query(stmt)
             .bind(ids)
             .execute(transaction).await?;
@@ -122,7 +122,7 @@ impl Dir {
 
     /// Return a count of the number of dirs in the database.
     pub async fn count(transaction: &mut Transaction<'_, Postgres>) -> Result<i64> {
-        let count: i64 = sqlx::query("SELECT COUNT(id) FROM dirs")
+        let count: i64 = sqlx::query("SELECT COUNT(id) FROM stash.dirs")
             .fetch_one(transaction)
             .await?
             .get(0);
@@ -144,7 +144,7 @@ impl NewDir {
     /// Create an entry for a directory in the database and return a `Dir`.
     /// Does not commit the transaction, you must do so yourself.
     pub async fn create(self, transaction: &mut Transaction<'_, Postgres>) -> Result<Dir> {
-        let query = "INSERT INTO dirs (mtime, birth_time, birth_version, birth_hostname)
+        let query = "INSERT INTO stash.dirs (mtime, birth_time, birth_version, birth_hostname)
                      VALUES ($1::timestamptz, $2::timestamptz, $3::smallint, $4::text)
                      RETURNING id";
         let row = sqlx::query(query)
@@ -209,7 +209,7 @@ impl File {
         if ids.is_empty() {
             return Ok(vec![])
         }
-        let query = "SELECT id, mtime, size, executable, birth_time, birth_version, birth_hostname, b3sum FROM files WHERE id = ANY($1::bigint[])";
+        let query = "SELECT id, mtime, size, executable, birth_time, birth_version, birth_hostname, b3sum FROM stash.files WHERE id = ANY($1::bigint[])";
         Ok(sqlx::query_as::<_, File>(query).bind(ids).fetch_all(transaction).await?)
     }
 
@@ -220,7 +220,7 @@ impl File {
 
     /// Set the b3sum for a file that may not have one already
     pub async fn set_b3sum(transaction: &mut Transaction<'_, Postgres>, file_id: i64, b3sum: &[u8; 32]) -> Result<()> {
-        let query = "UPDATE files SET b3sum = $1::bytea WHERE id = $2::bigint";
+        let query = "UPDATE stash.files SET b3sum = $1::bytea WHERE id = $2::bigint";
         sqlx::query(query)
             .bind(b3sum.as_ref())
             .bind(file_id)
@@ -233,7 +233,7 @@ impl File {
     /// Does not commit the transaction, you must do so yourself.
     pub async fn create(&self, transaction: &mut Transaction<'_, Postgres>) -> Result<()> {
         assert!(self.size >= 0, "size must be >= 0");
-        let query = "INSERT INTO files (id, mtime, size, executable, birth_time, birth_version, birth_hostname, b3sum)
+        let query = "INSERT INTO stash.files (id, mtime, size, executable, birth_time, birth_version, birth_hostname, b3sum)
                      OVERRIDING SYSTEM VALUE
                      VALUES ($1::bigint, $2::timestamptz, $3::bigint, $4::boolean, $5::timestamptz, $6::smallint, $7::text, $8::bytea)";
         sqlx::query(query)
@@ -252,7 +252,7 @@ impl File {
     /// Remove files with given `ids`.
     /// Does not commit the transaction, you must do so yourself.
     pub async fn remove(transaction: &mut Transaction<'_, Postgres>, ids: &[i64]) -> Result<()> {
-        let stmt = "DELETE FROM files WHERE id = ANY($1::bigint[])";
+        let stmt = "DELETE FROM stash.files WHERE id = ANY($1::bigint[])";
         sqlx::query(stmt)
             .bind(ids)
             .execute(transaction).await?;
@@ -261,7 +261,7 @@ impl File {
 
     /// Return a count of the number of files in the database.
     pub async fn count(transaction: &mut Transaction<'_, Postgres>) -> Result<i64> {
-        let count: i64 = sqlx::query("SELECT COUNT(id) FROM files")
+        let count: i64 = sqlx::query("SELECT COUNT(id) FROM stash.files")
             .fetch_one(transaction)
             .await?
             .get(0);
@@ -290,7 +290,7 @@ impl NewFile {
     /// Does not commit the transaction, you must do so yourself.
     pub async fn create(self, transaction: &mut Transaction<'_, Postgres>) -> Result<File> {
         assert!(self.size >= 0, "size must be >= 0");
-        let query = "INSERT INTO files (mtime, size, executable, birth_time, birth_version, birth_hostname, b3sum)
+        let query = "INSERT INTO stash.files (mtime, size, executable, birth_time, birth_version, birth_hostname, b3sum)
                      VALUES ($1::timestamptz, $2::bigint, $3::boolean, $4::timestamptz, $5::smallint, $6::text, $7::bytea)
                      RETURNING id";
         let row = sqlx::query(query)
@@ -353,14 +353,14 @@ impl Symlink {
         if ids.is_empty() {
             return Ok(vec![])
         }
-        let query = "SELECT id, mtime, target, birth_time, birth_version, birth_hostname FROM symlinks WHERE id = ANY($1::bigint[])";
+        let query = "SELECT id, mtime, target, birth_time, birth_version, birth_hostname FROM stash.symlinks WHERE id = ANY($1::bigint[])";
         Ok(sqlx::query_as::<_, Symlink>(query).bind(ids).fetch_all(transaction).await?)
     }
 
     /// Remove symlinks with given `ids`.
     /// Does not commit the transaction, you must do so yourself.
     pub async fn remove(transaction: &mut Transaction<'_, Postgres>, ids: &[i64]) -> Result<()> {
-        let stmt = "DELETE FROM symlinks WHERE id = ANY($1::bigint[])";
+        let stmt = "DELETE FROM stash.symlinks WHERE id = ANY($1::bigint[])";
         sqlx::query(stmt)
             .bind(ids)
             .execute(transaction).await?;
@@ -369,7 +369,7 @@ impl Symlink {
 
     /// Return a count of the number of symlinks in the database.
     pub async fn count(transaction: &mut Transaction<'_, Postgres>) -> Result<i64> {
-        let count: i64 = sqlx::query("SELECT COUNT(id) FROM symlinks")
+        let count: i64 = sqlx::query("SELECT COUNT(id) FROM stash.symlinks")
             .fetch_one(transaction)
             .await?
             .get(0);
@@ -393,7 +393,7 @@ impl NewSymlink {
     /// Create an entry for a symlink in the database and return a `Symlink`.
     /// Does not commit the transaction, you must do so yourself.
     pub async fn create(self, transaction: &mut Transaction<'_, Postgres>) -> Result<Symlink> {
-        let query = "INSERT INTO symlinks (mtime, target, birth_time, birth_version, birth_hostname)
+        let query = "INSERT INTO stash.symlinks (mtime, target, birth_time, birth_version, birth_hostname)
                      VALUES ($1::timestamptz, $2::text, $3::timestamptz, $4::smallint, $5::text)
                      RETURNING id";
         let row = sqlx::query(query)
@@ -653,7 +653,7 @@ pub(crate) mod tests {
             let pool = new_secondary_pool().await;
             for table in &["dirs", "files", "symlinks"] {
                 let mut transaction = pool.begin().await?;
-                assert_cannot_truncate(&mut transaction, table).await;
+                assert_cannot_truncate(&mut transaction, &format!("stash.{table}")).await;
             }
             Ok(())
         }
@@ -663,7 +663,7 @@ pub(crate) mod tests {
         async fn test_can_change_dir_mutables() -> Result<()> {
             let pool = new_primary_pool().await;
             let mut transaction = pool.begin().await?;
-            sqlx::query("UPDATE dirs SET mtime = now() WHERE id = $1::bigint").bind(&1i64).execute(&mut transaction).await?;
+            sqlx::query("UPDATE stash.dirs SET mtime = now() WHERE id = $1::bigint").bind(&1i64).execute(&mut transaction).await?;
             transaction.commit().await?;
             Ok(())
         }
@@ -676,7 +676,7 @@ pub(crate) mod tests {
             transaction.commit().await?;
             for (column, value) in &[("id", "100"), ("birth_time", "now()"), ("birth_version", "1"), ("birth_hostname", "'dummy'")] {
                 let mut transaction = pool.begin().await?;
-                let query = format!("UPDATE dirs SET {column} = {value} WHERE id = $1::bigint");
+                let query = format!("UPDATE stash.dirs SET {column} = {value} WHERE id = $1::bigint");
                 let result = sqlx::query(&query).bind(&1i64).execute(&mut transaction).await;
                 let msg = result.err().expect("expected an error").to_string();
                 if *column == "id" {
@@ -696,13 +696,13 @@ pub(crate) mod tests {
             let file = NewFile { size: 0, executable: false, mtime: Utc::now(), birth: Birth::here_and_now(), b3sum: None }.create(&mut transaction).await?;
             transaction.commit().await?;
             let mut transaction = pool.begin().await?;
-            sqlx::query("UPDATE files SET mtime = now() WHERE id = $1::bigint").bind(&file.id).execute(&mut transaction).await?;
+            sqlx::query("UPDATE stash.files SET mtime = now() WHERE id = $1::bigint").bind(&file.id).execute(&mut transaction).await?;
             transaction.commit().await?;
             let mut transaction = pool.begin().await?;
-            sqlx::query("UPDATE files SET size = 100000 WHERE id = $1::bigint").bind(&file.id).execute(&mut transaction).await?;
+            sqlx::query("UPDATE stash.files SET size = 100000 WHERE id = $1::bigint").bind(&file.id).execute(&mut transaction).await?;
             transaction.commit().await?;
             let mut transaction = pool.begin().await?;
-            sqlx::query("UPDATE files SET executable = true WHERE id = $1::bigint").bind(&file.id).execute(&mut transaction).await?;
+            sqlx::query("UPDATE stash.files SET executable = true WHERE id = $1::bigint").bind(&file.id).execute(&mut transaction).await?;
             transaction.commit().await?;
             Ok(())
         }
@@ -716,7 +716,7 @@ pub(crate) mod tests {
             transaction.commit().await?;
             for (column, value) in &[("id", "100"), ("birth_time", "now()"), ("birth_version", "1"), ("birth_hostname", "'dummy'")] {
                 let mut transaction = pool.begin().await?;
-                let query = format!("UPDATE files SET {column} = {value} WHERE id = $1::bigint");
+                let query = format!("UPDATE stash.files SET {column} = {value} WHERE id = $1::bigint");
                 let result = sqlx::query(&query).bind(&file.id).execute(&mut transaction).await;
                 let msg = result.err().expect("expected an error").to_string();
                 if *column == "id" {
@@ -736,7 +736,7 @@ pub(crate) mod tests {
             let symlink = NewSymlink { target: "old".into(), mtime: Utc::now(), birth: Birth::here_and_now() }.create(&mut transaction).await?;
             transaction.commit().await?;
             let mut transaction = pool.begin().await?;
-            sqlx::query("UPDATE symlinks SET mtime = now() WHERE id = $1::bigint").bind(&symlink.id).execute(&mut transaction).await?;
+            sqlx::query("UPDATE stash.symlinks SET mtime = now() WHERE id = $1::bigint").bind(&symlink.id).execute(&mut transaction).await?;
             transaction.commit().await?;
             Ok(())
         }
@@ -750,7 +750,7 @@ pub(crate) mod tests {
             transaction.commit().await?;
             for (column, value) in &[("id", "100"), ("target", "'new'"), ("birth_time", "now()"), ("birth_version", "1"), ("birth_hostname", "'dummy'")] {
                 let mut transaction = pool.begin().await?;
-                let query = format!("UPDATE symlinks SET {column} = {value} WHERE id = $1::bigint");
+                let query = format!("UPDATE stash.symlinks SET {column} = {value} WHERE id = $1::bigint");
                 let result = sqlx::query(&query).bind(&symlink.id).execute(&mut transaction).await;
                 let msg = result.err().expect("expected an error").to_string();
                 if *column == "id" {

src/db.rs
@@ -61,7 +61,7 @@ static PGPOOL: Lazy<Shared<Pin<Box<dyn Future<Output=PgPool> + Send>>>> = Lazy::
         panic!("Refusing to create pgpool to EXASTASH_POSTGRES_URI={} in tests", database_uri);
     }
 
-    new_pgpool(&database_uri, max_connections, "stash").await.unwrap()
+    new_pgpool(&database_uri, max_connections, "public").await.unwrap()
 }.boxed().shared());
 
 /// Return the global `PgPool`.  It must not be used in more than one tokio runtime.
@@ -138,7 +138,7 @@ pub mod tests {
     /// Return a new `PgPool` connected to the `pg_tmp` for most tests.
     /// We do not return a shared `PgPool` because each `#[tokio::test]` has its own tokio runtime.
     pub(crate) async fn new_primary_pool() -> PgPool {
-        new_pgpool(&*PRIMARY_POOL_URI, 4, "stash").await.unwrap()
+        new_pgpool(&*PRIMARY_POOL_URI, 4, "public").await.unwrap()
     }
 
     /// PgPool Future initialized once by the first caller
@@ -151,6 +151,6 @@ pub mod tests {
     /// Return a new `PgPool` connected to the pg_tmp for `TRUNCATE` tests.
     /// We do not return a shared `PgPool` because each `#[tokio::test]` has its own tokio runtime.
     pub(crate) async fn new_secondary_pool() -> PgPool {
-        new_pgpool(&*SECONDARY_POOL_URI, 4, "stash").await.unwrap()
+        new_pgpool(&*SECONDARY_POOL_URI, 4, "public").await.unwrap()
     }
 }

src/db/dirent.rs
@@ -65,7 +65,7 @@ impl Dirent {
     /// Does not commit the transaction, you must do so yourself.
     pub async fn create(&self, transaction: &mut Transaction<'_, Postgres>) -> Result<()> {
         let InodeTuple(child_dir, child_file, child_symlink) = self.child.into();
-        let stmt = "INSERT INTO dirents (parent, basename, child_dir, child_file, child_symlink)
+        let stmt = "INSERT INTO stash.dirents (parent, basename, child_dir, child_file, child_symlink)
                     VALUES ($1::bigint, $2::text, $3::bigint, $4::bigint, $5::bigint)";
         sqlx::query(stmt)
             .bind(self.parent)
@@ -80,7 +80,7 @@ impl Dirent {
     /// Remove this directory entry.
     /// Does not commit the transaction, you must do so yourself.
     pub async fn remove(&self, transaction: &mut Transaction<'_, Postgres>) -> Result<()> {
-        let stmt = "DELETE FROM dirents WHERE parent = $1::bigint AND basename = $2::text";
+        let stmt = "DELETE FROM stash.dirents WHERE parent = $1::bigint AND basename = $2::text";
         sqlx::query(stmt)
             .bind(self.parent)
             .bind(&self.basename)
@@ -91,7 +91,7 @@ impl Dirent {
     /// Remove a directory entry by `parent` and `basename`.
     /// Does not commit the transaction, you must do so yourself.
     pub async fn remove_by_parent_basename(transaction: &mut Transaction<'_, Postgres>, parent: i64, basename: &str) -> Result<()> {
-        let stmt = "DELETE FROM dirents WHERE parent = $1::bigint AND basename = $2::text";
+        let stmt = "DELETE FROM stash.dirents WHERE parent = $1::bigint AND basename = $2::text";
         sqlx::query(stmt)
             .bind(parent)
             .bind(basename)
@@ -102,7 +102,7 @@ impl Dirent {
     /// Remove a directory entry by `child_dir`.
     /// Does not commit the transaction, you must do so yourself.
     pub async fn remove_by_child_dir(transaction: &mut Transaction<'_, Postgres>, child_dir: i64) -> Result<()> {
-        let stmt = "DELETE FROM dirents WHERE child_dir = $1::bigint";
+        let stmt = "DELETE FROM stash.dirents WHERE child_dir = $1::bigint";
         sqlx::query(stmt)
             .bind(child_dir)
             .execute(transaction).await?;
@@ -113,7 +113,7 @@ impl Dirent {
     /// There is no error on missing parents.
     pub async fn find_by_parents(transaction: &mut Transaction<'_, Postgres>, parents: &[i64]) -> Result<Vec<Dirent>> {
         // `child_dir IS DISTINCT FROM 1` filters out the root directory self-reference
-        let query = "SELECT parent, basename, child_dir, child_file, child_symlink FROM dirents
+        let query = "SELECT parent, basename, child_dir, child_file, child_symlink FROM stash.dirents
                      WHERE parent = ANY($1::bigint[]) AND child_dir IS DISTINCT FROM 1";
         Ok(sqlx::query_as::<_, Dirent>(query).bind(parents).fetch_all(transaction).await?)
     }
@@ -121,7 +121,7 @@ impl Dirent {
     /// Return an `Option<Dirent>` if a `Dirent` exists with the given `parent` and `basename`.
     pub async fn find_by_parent_and_basename(transaction: &mut Transaction<'_, Postgres>, parent: i64, basename: &str) -> Result<Option<Dirent>> {
         // `child_dir IS DISTINCT FROM 1` filters out the root directory self-reference
-        let query = "SELECT parent, basename, child_dir, child_file, child_symlink FROM dirents
+        let query = "SELECT parent, basename, child_dir, child_file, child_symlink FROM stash.dirents
                      WHERE parent = $1::bigint AND child_dir IS DISTINCT FROM 1 AND basename = $2::text";
         let mut out = sqlx::query_as::<_, Dirent>(query).bind(parent).bind(basename).fetch_all(transaction).await?;
         Ok(out.pop())
@@ -129,7 +129,7 @@ impl Dirent {
 
     /// Return an `Option<Dirent>` if a `Dirent` exists with the given `child_dir`.
     pub async fn find_by_child_dir(transaction: &mut Transaction<'_, Postgres>, child_dir: i64) -> Result<Option<Dirent>> {
-        let query = "SELECT parent, basename, child_dir, child_file, child_symlink FROM dirents
+        let query = "SELECT parent, basename, child_dir, child_file, child_symlink FROM stash.dirents
                      WHERE child_dir = $1::bigint";
         let mut out = sqlx::query_as::<_, Dirent>(query).bind(child_dir).fetch_all(transaction).await?;
         Ok(out.pop())
@@ -137,7 +137,7 @@ impl Dirent {
 
     /// Return a count of the number of dirents in the database.
     pub async fn count(transaction: &mut Transaction<'_, Postgres>) -> Result<i64> {
-        let count: i64 = sqlx::query("SELECT COUNT(parent) FROM dirents")
+        let count: i64 = sqlx::query("SELECT COUNT(parent) FROM stash.dirents")
             .fetch_one(transaction)
             .await?
             .get(0);
@@ -290,7 +290,7 @@ pub(crate) mod tests {
 
             for (column, value) in &[("parent", "100"), ("basename", "'new'"), ("child_dir", "1"), ("child_file", "1"), ("child_symlink", "1")] {
                 let mut transaction = pool.begin().await?;
-                let query = format!("UPDATE dirents SET {column} = {value} WHERE parent = $1::bigint AND child_dir = $2::bigint");
+                let query = format!("UPDATE stash.dirents SET {column} = {value} WHERE parent = $1::bigint AND child_dir = $2::bigint");
                 let result = sqlx::query(&query).bind(1i64).bind(child_dir.id).execute(&mut transaction).await;
                 assert_eq!(
                     result.err().expect("expected an error").to_string(),
@@ -314,7 +314,7 @@ pub(crate) mod tests {
             transaction.commit().await?;
 
             let mut transaction = pool.begin().await?;
-            assert_cannot_truncate(&mut transaction, "dirents").await;
+            assert_cannot_truncate(&mut transaction, "stash.dirents").await;
 
             Ok(())
         }
@@ -386,7 +386,7 @@ pub(crate) mod tests {
                 let result = Dirent::new(parent.id, basename.to_string(), InodeId::Dir(child.id)).create(&mut transaction).await;
                 assert_eq!(
                     result.err().expect("expected an error").to_string(),
-                    "error returned from database: value for domain linux_basename violates check constraint \"linux_basename_check\""
+                    "error returned from database: value for domain stash.linux_basename violates check constraint \"linux_basename_check\""
                 );
             }
 

src/db/google_auth.rs
@@ -21,7 +21,7 @@ impl GoogleApplicationSecret {
     /// Create a google_application_secret in the database.
     /// Does not commit the transaction, you must do so yourself.
     pub async fn create(&self, transaction: &mut Transaction<'_, Postgres>) -> Result<()> {
-        let stmt = "INSERT INTO google_application_secrets (domain_id, secret) VALUES ($1::smallint, $2::jsonb)";
+        let stmt = "INSERT INTO stash.google_application_secrets (domain_id, secret) VALUES ($1::smallint, $2::jsonb)";
         sqlx::query(stmt)
             .bind(&self.domain_id)
             .bind(&self.secret)
@@ -32,14 +32,14 @@ impl GoogleApplicationSecret {
 
     /// Return a `Vec<GoogleApplicationSecret>` of all google_application_secrets.
     pub async fn find_all(transaction: &mut Transaction<'_, Postgres>) -> Result<Vec<GoogleApplicationSecret>> {
-        let query = "SELECT domain_id, secret FROM google_application_secrets";
+        let query = "SELECT domain_id, secret FROM stash.google_application_secrets";
         Ok(sqlx::query_as::<_, GoogleApplicationSecret>(query).fetch_all(transaction).await?)
     }
 
     /// Return a `Vec<GoogleApplicationSecret>` for the corresponding list of `domain_ids`.
     /// There is no error on missing domains.
     pub async fn find_by_domain_ids(transaction: &mut Transaction<'_, Postgres>, domain_ids: &[i16]) -> Result<Vec<GoogleApplicationSecret>> {
-        let query = "SELECT domain_id, secret FROM google_application_secrets WHERE domain_id = ANY($1::smallint[])";
+        let query = "SELECT domain_id, secret FROM stash.google_application_secrets WHERE domain_id = ANY($1::smallint[])";
         Ok(sqlx::query_as::<_, GoogleApplicationSecret>(query).bind(domain_ids).fetch_all(transaction).await?)
     }
 }
@@ -63,7 +63,7 @@ impl GoogleAccessToken {
     /// Create a google_access_token in the database.
     /// Does not commit the transaction, you must do so yourself.
     pub async fn create(&self, transaction: &mut Transaction<'_, Postgres>) -> Result<()> {
-        let stmt = "INSERT INTO google_access_tokens (owner_id, access_token, refresh_token, expires_at)
+        let stmt = "INSERT INTO stash.google_access_tokens (owner_id, access_token, refresh_token, expires_at)
                     VALUES ($1::int, $2::text, $3::text, $4::timestamptz)";
         sqlx::query(stmt)
             .bind(&self.owner_id)
@@ -79,7 +79,7 @@ impl GoogleAccessToken {
     /// There is no error if the owner does not exist.
     /// Does not commit the transaction, you must do so yourself.
     pub async fn delete(&self, transaction: &mut Transaction<'_, Postgres>) -> Result<()> {
-        let stmt = "DELETE FROM google_access_tokens WHERE owner_id = $1::int";
+        let stmt = "DELETE FROM stash.google_access_tokens WHERE owner_id = $1::int";
         sqlx::query(stmt).bind(&self.owner_id).execute(transaction).await?;
         Ok(())
     }
@@ -88,7 +88,7 @@ impl GoogleAccessToken {
     pub async fn find_by_expires_at(transaction: &mut Transaction<'_, Postgres>, expires_at: DateTime<Utc>) -> Result<Vec<GoogleAccessToken>> {
         Ok(sqlx::query_as::<_, GoogleAccessToken>(
             "SELECT owner_id, access_token, refresh_token, expires_at
-             FROM google_access_tokens
+             FROM stash.google_access_tokens
              WHERE expires_at < $1::timestamptz"
         ).bind(expires_at).fetch_all(transaction).await?)
     }
@@ -98,7 +98,7 @@ impl GoogleAccessToken {
     pub async fn find_by_owner_ids(transaction: &mut Transaction<'_, Postgres>, owner_ids: &[i32]) -> Result<Vec<GoogleAccessToken>> {
         Ok(sqlx::query_as::<_, GoogleAccessToken>(
             "SELECT owner_id, access_token, refresh_token, expires_at
-             FROM google_access_tokens
+             FROM stash.google_access_tokens
              WHERE owner_id = ANY($1::int[])"
         ).bind(owner_ids).fetch_all(transaction).await?)
     }
@@ -142,7 +142,7 @@ impl GoogleServiceAccount {
     pub async fn create(&self, transaction: &mut Transaction<'_, Postgres>) -> Result<()> {
         let k = &self.key;
         let stmt =
-            "INSERT INTO google_service_accounts (
+            "INSERT INTO stash.google_service_accounts (
                 owner_id, client_email, client_id, project_id, private_key_id, private_key,
                 auth_uri, token_uri, auth_provider_x509_cert_url, client_x509_cert_url)
              VALUES ($1::int, $2::text, $3::text, $4::text, $5::text, $6::text, $7::text, $8::text, $9::text, $10::text)";
@@ -172,7 +172,7 @@ impl GoogleServiceAccount {
         let query = format!("
             SELECT owner_id, client_email, client_id, project_id, private_key_id, private_key,
                    auth_uri, token_uri, auth_provider_x509_cert_url, client_x509_cert_url
-            FROM google_service_accounts
+            FROM stash.google_service_accounts
             WHERE owner_id = ANY($1::int[])
             {limit_sql}
         ");

commit 5a0f1c2d993f433c97ae7f21afa4614b904ac640

inode: fix a regression introduced in 9cfd9051623efd86cfcaac7c649c69bb39e5b1f0 that prevents the creation of new files

src/db/inode.rs
@@ -215,7 +215,7 @@ impl File {
 
     /// Return a new, unique id for a file.  Caller can take this id and `create()` a `File` with it later.
     pub async fn next_id(transaction: &mut Transaction<'_, Postgres>) -> Result<i64> {
-        db::nextval(transaction, "files_id_seq").await
+        db::nextval(transaction, "stash.files_id_seq").await
     }
 
     /// Set the b3sum for a file that may not have one already