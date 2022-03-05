fc9855ff2ee9c6e99966bf34150b5e8eaecff704
Ivan Kozik <ivan@ludios.org>
2022-03-05 08:23:51 +0000

    WIP storage_fofs

--- src/db/storage/fofs.rs
+++ src/db/storage/fofs.rs
@@ -5,24 +5,53 @@ use anyhow::Result;
 use sqlx::{Postgres, Transaction};
 use serde::Serialize;
 
-/// A new cell entity
-#[derive(Debug, Copy, Clone, PartialEq, Eq, Serialize)]
-pub struct NewCell {
-    /// The pile we are parented in
-    pile_id: i32,
+/// A pile entity
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, sqlx::FromRow)]
+pub struct Pile {
+    /// Unique pile id
+    id: i32,
+    /// The machine on which the pile is stored
+    hostname: String,
+    /// The absolute path to the root directory of the pile on the machine
+    path: String,
 }
 
-impl NewCell {
-    /// Create an cell entity in the database.
+impl Pile {
+    /// Return a `Vec<Pile>` for the corresponding list of pile `ids`.
+    /// There is no error on missing piles.
+    pub async fn find_by_ids(transaction: &mut Transaction<'_, Postgres>, ids: &[i32]) -> Result<Vec<Pile>> {
+        if ids.is_empty() {
+            return Ok(vec![])
+        }
+        let piles = sqlx::query_as!(Pile, "
+            SELECT id, hostname, path FROM stash.piles WHERE id = ANY($1)", ids
+        ).fetch_all(transaction).await?;
+        Ok(piles)
+    }
+}
+
+/// A new pile entity
+#[derive(Debug, Clone, PartialEq, Eq, Serialize, sqlx::FromRow)]
+pub struct NewPile {
+    /// The machine on which the pile is stored
+    hostname: String,
+    /// The absolute path to the root directory of the pile on the machine
+    path: String,
+}
+
+impl NewPile {
+    /// Create an pile entity in the database.
     /// Does not commit the transaction, you must do so yourself.
     pub async fn create(&self, transaction: &mut Transaction<'_, Postgres>) -> Result<()> {
-        sqlx::query!("INSERT INTO stash.cells (pile_id) VALUES ($1)", self.pile_id).execute(transaction).await?;
+        sqlx::query!("INSERT INTO stash.piles (hostname, path) VALUES ($1::text, $2)", self.hostname, self.path)
+            .execute(transaction).await?;
         Ok(())
     }
 }
 
+
+
 /// A cell entity
-/// A storage_fofs entity
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Serialize, sqlx::FromRow)]
 pub struct Cell {
     /// Unique cell id
@@ -42,6 +71,24 @@ impl Cell {
         Ok(())
     }
 }
+
+/// A new cell entity
+#[derive(Debug, Copy, Clone, PartialEq, Eq, Serialize)]
+pub struct NewCell {
+    /// The pile we are parented in
+    pile_id: i32,
+}
+
+impl NewCell {
+    /// Create an cell entity in the database.
+    /// Does not commit the transaction, you must do so yourself.
+    pub async fn create(&self, transaction: &mut Transaction<'_, Postgres>) -> Result<()> {
+        sqlx::query!("INSERT INTO stash.cells (pile_id) VALUES ($1)", self.pile_id).execute(transaction).await?;
+        Ok(())
+    }
+}
+
+
 
 /// A storage_fofs entity
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Serialize, sqlx::FromRow)]
