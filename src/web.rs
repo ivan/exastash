4a0a50d085e0cf2e78683dc91cb4f6662d0f5bf5
Ivan Kozik <ivan@ludios.org>
2022-05-18 02:18:44 +0000

    WIP web; make our first axum request handler actually work

--- src/web.rs
+++ src/web.rs
@@ -1,24 +1,18 @@
 //! web server for exastash
 
-use anyhow::{anyhow, ensure, Error, Result};
 use axum::{
     routing::{get, post},
     extract::Path,
     http::StatusCode,
     response::{Response, IntoResponse},
-    Json, Router,
+    Router,
 };
 use axum_macros::debug_handler;
-use serde::{Deserialize, Serialize};
 use std::net::SocketAddr;
 use crate::util;
 
-mod errors {
-
-}
-
 /// Start a web server with fofs serving capabilities
-pub async fn run(port: u16) -> Result<()> {
+pub async fn run(port: u16) -> Result<(), hyper::Error> {
     // build our application with a route
     let app = Router::new()
         .route("/", get(root))
@@ -28,38 +22,66 @@ pub async fn run(port: u16) -> Result<()> {
     tracing::info!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
-        .await?;
-    Ok(())
+        .await
 }
 
 async fn root() -> String {
     format!("es web on {}", util::get_hostname())
 }
 
-struct AppError(anyhow::Error);
+/// Errors used by our web server
+#[derive(thiserror::Error, Debug)]
+#[allow(variant_size_differences)]
+pub enum Error {
+    /// Access forbidden
+    #[error("access forbidden")]
+    Forbidden,
 
-impl From<Error> for AppError {
-    fn from(inner: Error) -> Self {
-        AppError(inner)
+    /// Bad request
+    #[error("bad request")]
+    BadRequest,
+
+    /// A problem with the database
+    #[error("an error occurred with the database")]
+    Sqlx(#[from] sqlx::Error),
+
+    /// Some other error created by anyhow
+    #[error("an internal server error occurred")]
+    Anyhow(#[from] anyhow::Error),
+}
+
+impl Error {
+    fn status_code(&self) -> StatusCode {
+        match self {
+            Self::Forbidden => StatusCode::FORBIDDEN,
+            Self::BadRequest => StatusCode::BAD_REQUEST,
+            Self::Sqlx(_) | Self::Anyhow(_) => StatusCode::INTERNAL_SERVER_ERROR,
+        }
     }
 }
 
-impl IntoResponse for AppError {
+impl IntoResponse for Error {
     fn into_response(self) -> Response {
-        (StatusCode::INTERNAL_SERVER_ERROR, self.0.to_string()).into_response()
+        // Log some errors
+        match self {
+            Self::Sqlx(ref e) => {
+                log::error!("SQLx error: {:?}", e);
+            }
+            Self::Anyhow(ref e) => {
+                log::error!("Generic error: {:?}", e);
+            }
+            _ => (),
+        }
+        (self.status_code(), self.to_string()).into_response()
     }
 }
-
-type AppResult = std::result::Result<Response, AppError>;
 
 #[debug_handler]
 async fn fofs_get(
-    Path(pile_id): Path<i64>,
-    Path(cell_id): Path<i64>,
-    Path(file_id): Path<i64>,
-) -> AppResult {
-    try {
-        ensure!(pile_id >= 1, "pile_id was {}", pile_id);
-        Ok((StatusCode::OK, "OK").into_response())
-    }?
+    Path((pile_id, cell_id, file_id)): Path<(i64, i64, i64)>,
+) -> Result<Response, Error> {
+    if pile_id < 1 || cell_id < 1 || file_id < 1 {
+        return Err(Error::BadRequest);
+    }
+    Ok((StatusCode::OK, "OK").into_response())
 }
