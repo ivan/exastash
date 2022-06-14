ee91eba472cbadc811d0bb2af1621f44c494a7f6
Ivan Kozik <ivan@ludios.org>
2022-06-14 21:57:51 +0000

    3.0.1

--- schema/exastash_versions.sql
+++ schema/exastash_versions.sql
@@ -81,6 +81,7 @@ INSERT INTO exastash_versions (id, version) VALUES
     (72, '2.5.2'),
     (73, '2.5.3'),
     (74, '2.5.4'),
-    (75, '3.0.0');
+    (75, '3.0.0'),
+    (76, '3.0.1');
 
 -- Remember to update src/lib.rs and schema/inodes.sql after adding an exastash version
