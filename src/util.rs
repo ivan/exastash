f28fa22418459a5cce7302e54191535f7a8fbed1
Ivan Kozik <ivan@ludios.org>
2022-07-24 06:07:10 +0000

    util: make get_hostname public so that tubekit_web can use it

--- src/util.rs
+++ src/util.rs
@@ -23,7 +23,8 @@ pub(crate) fn env_var(var: &str) -> Result<String> {
     env::var(var).with_context(|| anyhow!("Could not get variable {:?} from environment", var))
 }
 
-pub(crate) fn get_hostname() -> String {
+/// Get this machine's hostname
+pub fn get_hostname() -> String {
     let os_string = gethostname::gethostname();
     let hostname = os_string.to_str().expect("hostname on this machine was not valid UTF-8");
     hostname.to_owned()
