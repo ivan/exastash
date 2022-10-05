a923e62932a68f008d227dd4a354e7b20bff0416
Ivan Kozik <ivan@ludios.org>
2022-10-05 18:43:34 +0000

    schema: also use pgroonga extension; add comment about pg_catalog.pg_ivm_immv

--- schema/extensions.sql
+++ schema/extensions.sql
@@ -1,2 +1,8 @@
 CREATE EXTENSION IF NOT EXISTS periods VERSION '1.2' CASCADE;
+-- GRANT USAGE ON SCHEMA periods TO archive;
+
 CREATE EXTENSION IF NOT EXISTS pg_ivm VERSION '1.2' CASCADE;
+-- https://github.com/sraoss/pg_ivm/issues/25
+-- GRANT ALL ON TABLE pg_catalog.pg_ivm_immv TO archive;
+
+CREATE EXTENSION IF NOT EXISTS pgroonga;
