4ba6b1ba6d63535072b603a85c794b43c4430c5b
Ivan Kozik <ivan@ludios.org>
2022-10-05 18:44:34 +0000

    schema: use pg_ivm 1.3

--- schema/extensions.sql
+++ schema/extensions.sql
@@ -1,8 +1,6 @@
 CREATE EXTENSION IF NOT EXISTS periods VERSION '1.2' CASCADE;
 -- GRANT USAGE ON SCHEMA periods TO archive;
 
-CREATE EXTENSION IF NOT EXISTS pg_ivm VERSION '1.2' CASCADE;
--- https://github.com/sraoss/pg_ivm/issues/25
--- GRANT ALL ON TABLE pg_catalog.pg_ivm_immv TO archive;
+CREATE EXTENSION IF NOT EXISTS pg_ivm VERSION '1.3' CASCADE;
 
 CREATE EXTENSION IF NOT EXISTS pgroonga;
